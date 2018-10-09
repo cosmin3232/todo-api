f9c3ccfde256346c682f801ad2f1d8631ed1218f
cosmin3232 <cosmin3232@yahoo.com>
2018-10-09 19:34:09 +0300

    changes

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
+/* var todos = [{
 	id: 1,
-	description: 'Meet mom for lunch',
+	description: 'Meet mom for lunch at 09 AM',
 	completed: false
 }, {
 	id: 2,
@@ -14,10 +15,16 @@ var todos = [{
 	description: 'Make money',
 	completed: true
 }];
+*/
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
 	res.send('Todo API Root');
-});
+}); 
+
 
 // GET /todos
 app.get('/todos', function(req, res) {
@@ -40,8 +47,19 @@ app.get('/todos/:id', function(req, res) {
 		res.status(404).send();
 	}
 
-	res.send('Asking for todo with id of: ' + req.params.id);
+	res.send('Asking for todo with id of  s: ' + req.params.id);
 });
+
+// POST /todos
+app.post('/todos', function(req, res) {
+	var body = req.body;
+
+	body.id = todoNextId++;
+	todos.push(body);
+
+	res.json(body);
+});
+
 
 
 app.listen(PORT, function() {
